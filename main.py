b2b22a4ce7b0893f113f36e5ed3828a0fdb8bdbd
Sergio <serfernandezser@gmail.com>
2021-10-05 10:58:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from window import *
+
+import sys
+
+
+
+class Main (QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui= Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ =="__main__":
+    app=QtWidgets.QApplication([])
+    window=Main()
+    window.show()
+    sys.exit(app.exec())
